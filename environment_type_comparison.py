ea444f31120200beb30bdf0352db24cf923dc6cd
MT Jefferson <mtj507@login1.pri.viking.alces.network>
2019-11-21 15:30:37 +0000

    medians and annotations added

--- environment_type_comparison.py
+++ environment_type_comparison.py
@@ -7,9 +7,11 @@ import numpy as np
 import openaq
 from datetime import datetime
 from matplotlib.backends.backend_pdf import PdfPages
+import warnings
+#prevent warning when slicing ddf by headers
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-
-metadata_csv='/users/mtj507/scratch/defra_data/defra_eng_site_metadata.csv'
+metadata_csv='/users/mtj507/scratch/defra_data/defra_site_metadata.csv'
 metadata=pd.read_csv(metadata_csv, low_memory=False)
 environment=metadata['Environment Type']
 environments=pd.unique(environment)
@@ -19,7 +21,7 @@ emissions=['no2', 'no', 'pm25', 'o3']
 
 
 for environment in environments:
-    metadata_csv='/users/mtj507/scratch/defra_data/defra_eng_site_metadata.csv'
+    metadata_csv='/users/mtj507/scratch/defra_data/defra_site_metadata.csv'
     metadata=pd.read_csv(metadata_csv, low_memory=False)
     metadata=metadata.loc[metadata['Environment Type']==environment]
     check=pd.unique(metadata['Environment Type'])
@@ -34,7 +36,7 @@ for environment in environments:
     
  
     for i in emissions:
-        defra_csv='/users/mtj507/scratch/defra_data/defra_'+i+'_eng_2019.csv'
+        defra_csv='/users/mtj507/scratch/defra_data/defra_'+i+'_uk_2019.csv'
         print(i)
         ddf=pd.read_csv(defra_csv, low_memory=False)
         ddf.index=pd.to_datetime(ddf['Date'], dayfirst=True)+pd.to_timedelta(ddf['Time'])
@@ -55,13 +57,22 @@ for environment in environments:
         ddf1=ddf1.dropna(axis=1,how='all')
         if (environment == 'Industrial Suburban' and i == 'pm25'):
           continue 
-        ddf_mean=ddf1.groupby('hour').mean()
-        ddf_mean['mean']=ddf_mean.mean(axis=1)       
-        ddf_std=ddf1.groupby('hour').std()
-        ddf_std['std']=ddf_std.mean(axis=1)
-        plt.plot(ddf_mean.index, ddf_mean['mean'], label='Observation', color='blue')
-        plt.fill_between(ddf_mean.index, (ddf_mean['mean']+ddf_std['std']), (ddf_mean['mean']-ddf_std['std']), alpha=0.5, facecolor='turquoise', edgecolor='deepskyblue')
+        ddf_median=ddf1.groupby('hour').median()
+        ddf_median['median']=ddf_median.mean(axis=1)
+        ddf_Q1=ddf1.groupby('hour').quantile(0.25)
+        ddf_Q1['Q1']=ddf_Q1.mean(axis=1)
+        ddf_Q3=ddf1.groupby('hour').quantile(0.75)
+        ddf_Q3['Q3']=ddf_Q3.mean(axis=1)
+        plt.plot(ddf_median.index, ddf_median['median'], label='Observation', color='blue')
+        plt.fill_between(ddf_median.index, ddf_Q1['Q1'], ddf_Q3['Q3'], alpha=0.5, facecolor='turquoise', edgecolor='deepskyblue')
 
+        obs_median=ddf_median['median'].mean()
+        obs_median=str(round(obs_median,2))
+        obs_Q1=ddf_Q1['Q1'].mean()
+        obs_Q1=str(round(obs_Q1,2))
+        obs_Q3=ddf_Q3['Q3'].mean()
+        obs_Q3=str(round(obs_Q3,2))
+      
         days_of_data=len(pd.unique(ddf['day and month']))
         dates=pd.unique(ddf['day and month'])
         mod_data = np.zeros((24,days_of_data,no_locations))
@@ -116,7 +127,19 @@ for environment in environments:
         plt.ylabel(i + ' ug/m3')
         plt.legend()
         plt.title(environment +' '+ i)
-        path='/users/mtj507/scratch/obs_vs_forecast/plots/environments/'
+
+        nasa_median=np.median(mod_data)
+        nasa_median=str(round(nasa_median,2))
+        nasa_Q1=np.percentile(mod_data,25)
+        nasa_Q1=str(round(nasa_Q1,2))
+        nasa_Q3=np.percentile(mod_data,75)
+        nasa_Q3=str(round(nasa_Q3,2))
+
+
+        text=' Obs median = ' + obs_median + ' ug/m3 \n Obs IQR = ' + obs_Q1 + ' - ' + obs_Q3 + ' ug/m3 \n Forecast median = ' + nasa_median + ' ug/m3 \n Forecast IQR = ' + nasa_Q1 + ' - ' + nasa_Q3 + ' ug/m3'
+        plt.annotate(text, fontsize=7, xy=(0.01, 0.85), xycoords='axes fraction')
+
+        path='/users/mtj507/scratch/obs_vs_forecast/plots/environments/full_week/'
         plt.savefig(path+environment+'_'+i)
         print('saved')
         plt.close() 
