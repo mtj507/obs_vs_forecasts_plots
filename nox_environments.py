2864ab92718446d2e9df4f349a15a70900925f6c
MT Jefferson <mtj507@login2.pri.viking.alces.network>
2020-01-21 14:07:47 +0000

    added rmse

--- nox_environments.py
+++ nox_environments.py
@@ -78,12 +78,14 @@ for environment in environments:
     df=df.groupby(df.columns, axis=1).sum()
     df=df.loc[:,nox_locations]
     df['hour']=df.index.hour
-    df_mean=df.groupby('hour').mean()
-    df_mean['mean']=df_mean.mean(axis=1)
-    df_std=df.groupby('hour').std()
-    df_std['std']=df_std.std(axis=1)
-    plt.plot(df_mean.index, df_mean['mean'], label='Observation', color='blue')
-    plt.fill_between(df_mean.index, (df_mean['mean']+df_std['std']), (df_mean['mean']-df_std['std']), alpha=0.5, facecolor='turquoise', edgecolor='deepskyblue')
+    df_median=df.groupby('hour').median()
+    df_median['median']=df_median.mean(axis=1)
+    df_Q1=df.groupby('hour').quantile(0.25)
+    df_Q1['Q1']=df_Q1.mean(axis=1)
+    df_Q3=df.groupby('hour').quantile(0.75)
+    df_Q3['Q3']=df_Q3.mean(axis=1)
+    plt.plot(df_median.index, df_median['median'], label='Observation', color='blue')
+    plt.fill_between(df_median.index, df_Q1['Q1'], df_Q3['Q3'], alpha=0.5, facecolor='turquoise', edgecolor='deepskyblue')
   
 
     metadata_csv='/users/mtj507/scratch/defra_data/defra_site_metadata.csv'
@@ -140,7 +142,26 @@ for environment in environments:
     plt.ylabel('NOx' + ' ug/m3')
     plt.legend()
     plt.title(environment +' '+ 'NOx')
-    path='/users/mtj507/scratch/obs_vs_forecast/plots/environments/'
+
+    nasa_median=np.median(mod_data)
+    nasa_median=str(round(nasa_median,2))
+    nasa_Q1=np.percentile(mod_data,25)
+    nasa_Q1=str(round(nasa_Q1,2))
+    nasa_Q3=np.percentile(mod_data,75)
+    nasa_Q3=str(round(nasa_Q3,2))
+
+    def rmse(predictions, targets):
+       return np.sqrt(((predictions-targets)**2).mean())
+
+    rmse_val=rmse(np.median(mod_data,axis=(1,2)),df_median['median'])
+    rmse_txt=str(round(rmse_val,2))
+
+    text=' RMSE = '+rmse_txt+' ug/m3'
+#    text=' Obs median = ' + obs_median + ' ug/m3 \n Obs IQR = ' + obs_Q1 + ' - ' + obs_Q3 + ' ug/m3 \n Forecast median = ' + nasa_median + ' ug/m3 \n Forecast IQR = ' + nasa_Q1 + ' - ' + nasa_Q3 + ' ug/m3'
+    plt.annotate(text, fontsize=7, xy=(0.01, 0.85), xycoords='axes fraction')
+
+
+    path='/users/mtj507/scratch/obs_vs_forecast/plots/environments/full_week/'
     plt.savefig(path+environment+'_'+'nox')
     print('saved')
     plt.close() 
