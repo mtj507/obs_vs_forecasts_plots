b9123e5d681c9c764271ae64118cd5a525f040c1
MT Jefferson <mtj507@login2.pri.viking.alces.network>
2020-01-22 16:04:05 +0000

    Got code functioning

--- aurn_scatter.py
+++ aurn_scatter.py
@@ -56,8 +56,6 @@ environments=pd.unique(environment)
 plt.xlabel('Observation Median  ug/m3')
 plt.ylabel('Forecast Median  ug/m3')
 plt.title(emission+' '+week)
-xy=np.linspace(*plt.xlim())
-plt.plot(xy,xy,linestyle='dashed',color='grey')
 
 aurn_list=[]
 aurn_Q1_list=[]
@@ -248,7 +246,7 @@ for environment in environments:
 
         if environment == 'Industrial Suburban':
             obs_IS_list.append(obs_median)
-            obs_IS_Q3_list.append(obs_Q1)
+            obs_IS_Q1_list.append(obs_Q1)
             obs_IS_Q3_list.append(obs_Q3)
             mod_IS_list.append(nasa_median)
             mod_IS_Q1_list.append(nasa_Q1)
@@ -262,21 +260,147 @@ def linear_func(p, x):
     return y
 
 
-aurn_data={'obs':aurn_list,'obs Q1':aurn_Q1_list,'obs Q3':aurn_Q3_list,'model':mod_list,'model Q1':mod_Q1_list,'model Q3':mod_Q3_list}
+aurn_data={'obs':aurn_list,'obs Q1':aurn_Q1_list,'obs Q3':aurn_Q3_list,'mod':mod_list,'mod Q1':mod_Q1_list,'mod Q3':mod_Q3_list}
 aurn_df=pd.DataFrame(aurn_data)
 aurn_df=aurn_df[aurn_df > 0].dropna()
 aurn_df=aurn_df.dropna()
 aurn_df['obs_err']=aurn_df['obs Q3']-aurn_df['obs Q1']
-aurn_df['mod_err']=aurn_df['model Q3']-aurn_df['model Q1']
+aurn_df['mod_err']=aurn_df['mod Q3']-aurn_df['mod Q1']
 aurn_df=aurn_df.reset_index(drop=True)
 
+bu_data={'obs':obs_BU_list,'obs Q1':obs_BU_Q1_list,'obs Q3':obs_BU_Q3_list,'mod':mod_BU_list,'mod Q1':mod_BU_Q1_list,'mod Q3':mod_BU_Q3_list}
+bu_df=pd.DataFrame(bu_data)
+bu_df=bu_df[bu_df > 0].dropna()
+bu_df=bu_df.dropna()
+bu_df['obs_err']=bu_df['obs Q3']-bu_df['obs Q1']
+bu_df['mod_err']=bu_df['mod Q3']-bu_df['mod Q1']
+bu_df=bu_df.reset_index(drop=True)
+
+br_data={'obs':obs_BR_list,'obs Q1':obs_BR_Q1_list,'obs Q3':obs_BR_Q3_list,'mod':mod_BR_list,'mod Q1':mod_BR_Q1_list,'mod Q3':mod_BR_Q3_list}
+br_df=pd.DataFrame(br_data)
+br_df=br_df[br_df > 0].dropna()
+br_df=br_df.dropna()
+br_df['obs_err']=br_df['obs Q3']-br_df['obs Q1']
+br_df['mod_err']=br_df['mod Q3']-br_df['mod Q1']
+br_df=br_df.reset_index(drop=True)
+
+tu_data={'obs':obs_TU_list,'obs Q1':obs_TU_Q1_list,'obs Q3':obs_TU_Q3_list,'mod':mod_TU_list,'mod Q1':mod_TU_Q1_list,'mod Q3':mod_TU_Q3_list}
+tu_df=pd.DataFrame(tu_data)
+tu_df=tu_df[tu_df > 0].dropna()
+tu_df=tu_df.dropna()
+tu_df['obs_err']=tu_df['obs Q3']-tu_df['obs Q1']
+tu_df['mod_err']=tu_df['mod Q3']-tu_df['mod Q1']
+tu_df=tu_df.reset_index(drop=True)
+
+iu_data={'obs':obs_IU_list,'obs Q1':obs_IU_Q1_list,'obs Q3':obs_IU_Q3_list,'mod':mod_IU_list,'mod Q1':mod_IU_Q1_list,'mod Q3':mod_IU_Q3_list}
+iu_df=pd.DataFrame(iu_data)
+iu_df=iu_df[iu_df > 0].dropna()
+iu_df=iu_df.dropna()
+iu_df['obs_err']=iu_df['obs Q3']-iu_df['obs Q1']
+iu_df['mod_err']=iu_df['mod Q3']-iu_df['mod Q1']
+iu_df=iu_df.reset_index(drop=True)
+
+bs_data={'obs':obs_BS_list,'obs Q1':obs_BS_Q1_list,'obs Q3':obs_BS_Q3_list,'mod':mod_BS_list,'mod Q1':mod_BS_Q1_list,'mod Q3':mod_BS_Q3_list}
+bs_df=pd.DataFrame(bs_data)
+bs_df=bs_df[bs_df > 0].dropna()
+bs_df=bs_df.dropna()
+bs_df['obs_err']=bs_df['obs Q3']-bs_df['obs Q1']
+bs_df['mod_err']=bs_df['mod Q3']-bs_df['mod Q1']
+bs_df=bs_df.reset_index(drop=True)
+
+is_data={'obs':obs_IS_list,'obs Q1':obs_IS_Q1_list,'obs Q3':obs_IS_Q3_list,'mod':mod_IS_list,'mod Q1':mod_IS_Q1_list,'mod Q3':mod_IS_Q3_list}
+is_df=pd.DataFrame(is_data)
+is_df=is_df[is_df > 0].dropna()
+is_df=is_df.dropna()
+is_df['obs_err']=is_df['obs Q3']-is_df['obs Q1']
+is_df['mod_err']=is_df['mod Q3']-is_df['mod Q1']
+is_df=is_df.reset_index(drop=True)
 
 
+envs=['aurn','bu','br','tu','iu','bs','is']
 
+for x in envs:
+    if x == 'aurn':
+      sdf=aurn_df
+      lbl='AURN'
+      clr='black'
+      obslist=aurn_list
+      modlist=mod_list
+    if x == 'bu':
+      sdf=bu_df
+      lbl='Background Urban'
+      clr='red'
+      obslist=obs_BU_list
+      modlist=mod_BU_list
+    if x == 'br':
+      sdf=br_df
+      lbl='Background Rural'
+      clr='blue'
+      obslist=obs_BR_list
+      modlist=mod_BR_list
+    if x == 'tu':
+      sdf=tu_df
+      lbl='Traffic Urban'
+      clr='green'
+      obslist=obs_TU_list
+      modlist=mod_TU_list
+    if x == 'iu':
+      sdf=iu_df
+      lbl='Industrial Urban'
+      clr='grey'
+      obslist=obs_IU_list
+      modlist=mod_IU_list
+    if x == 'bs':
+      sdf=bs_df
+      lbl='Background Suburban'
+      clr='brown'
+      obslist=obs_BS_list
+      modlist=mod_BS_list
+    if x == 'is':
+      sdf=is_df
+      lbl='Industrial Suburban'
+      clr='orange'
+      obslist=obs_IS_list
+      modlist=mod_IS_list
 
+    if sdf.empty:
+      continue
+    else:
+      x_data=sdf['obs']
+      y_data=sdf['mod']
+      x_err=sdf['obs_err']
+      y_err=sdf['mod_err']
 
+      linear=Model(linear_func)
+      datas=RealData(x_data,y_data,sx=x_err,sy=y_err)
+      odr=ODR(datas,linear,beta0=[0])
+      output=odr.run()
+      #output.pprint()
+      beta=output.beta
+      betastd=output.sd_beta
 
+      if len(sdf) > 4:
+        plt.plot(x_data,linear_func(beta,x_data),alpha=1,color=clr)
 
+      textbeta=float(output.beta)
+      textbeta=str(round(textbeta,3))
+      textbetastd=float(output.sd_beta)
+      textbetastd=str(round(textbetastd,3))
+      sites=str(len(x_data))
+      print(x) 
+      print('beta = '+textbeta)
+      print('std = '+textbetastd)
+      print('no of sites = '+sites)
+
+      plt.scatter(obslist,modlist,color=clr,label=lbl,marker='x',alpha=0.7) 
+      plt.legend(fontsize='x-small')
+
+xy=np.linspace(*plt.xlim())
+plt.plot(xy,xy,linestyle='dashed',color='grey')
+
+path='/users/mtj507/scratch/obs_vs_forecast/plots/scatter/'
+plt.savefig(path+emission+'_'+week+'.png')
+plt.close()
 
 
 
