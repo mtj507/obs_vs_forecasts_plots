0e30a263e3e8512f0588b199faf8da1aee8859ab
MT Jefferson <mtj507@login1.pri.viking.alces.network>
2020-02-11 14:58:39 +0000

    added colors

--- assimilation_scripts/aurn_map.py
+++ assimilation_scripts/aurn_map.py
@@ -9,26 +9,40 @@ import xarray as xr
 import matplotlib as mp1
 import cartopy.feature as cfeature
 
-fig=plt.figure()
-ax=fig.add_subplot(1,1,1,projection=ccrs.PlateCarree())
-ax.coastlines(resolution='50m')
-ax.add_feature(cfeature.OCEAN)
-ax.add_feature(cfeature.LAND)
 
 env_list=['Background Urban','Background Rural','Traffic Urban','Industrial Urban','Industrial Suburban','Background Suburban']
 
-emissions=['no2','no','o3','pm25']
+emissions=['Nitrogen dioxide','Nitric oxide','Ozone','PM2.5']
 
-metadata_csv='/users/mtj507/scratch/defra_data/defra_site_metadata.csv'
-metadata=pd.read_csv(metadata_csv, low_memory=False)
+for x in range(0,len(emissions)):
+    emission=emissions[x]
+    metadata_csv='/users/mtj507/scratch/defra_data/defra_site_metadata.csv'
+    metadata=pd.read_csv(metadata_csv, low_memory=False)
+    metadata=metadata.loc[metadata['AURN Pollutants Measured'].str.contains(emission)]
 
-for i in range(0,len(env_list)):
-    env_type=env_list[i]
-    print(env_type)
-    meta=metadata.loc[metadata['Environment Type']==env_type]
-    meta=meta.reset_index(drop=False)  
-    latitude=meta['Latitude']
-    longitude=meta['Longitude']
-    plt.scatter(longitude,latitude,marker='x',label=env_type)
-    plt.legend()
-    plt.show()
+    fig=plt.figure(figsize=[10,10])
+    ax=fig.add_subplot(1,1,1,projection=ccrs.PlateCarree())
+    ax.coastlines(resolution='50m')
+    ax.add_feature(cfeature.LAND.with_scale('50m'))
+    ax.add_feature(cfeature.OCEAN.with_scale('50m'))
+
+    for i in range(0,len(env_list)):
+        env_type=env_list[i]
+#        print(env_type)
+        meta=metadata.loc[metadata['Environment Type']==env_type]
+        meta=meta.reset_index(drop=False)  
+        latitude=meta['Latitude']
+        longitude=meta['Longitude']
+        plt.scatter(longitude,latitude,marker='x',label=env_type)
+        plt.legend()
+#        plt.show()
+
+    ax.set_title(emission+' site map')
+    path='/users/mtj507/scratch//obs_vs_forecast/assimilation_scripts/plots/maps/'    
+    plt.savefig(path+emission+'_site_map.png')
+#    plt.cla()
+#    print('saved')
+
+
+
+
